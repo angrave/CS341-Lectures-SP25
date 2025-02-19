6972893cdbd19b80f4ba2928b8d5e113f0cae41f
Lawrence Angrave <angrave@illinois.edu>
2025-02-19 13:41:21 -0600

    lec10

--- code/lec10/myrealloc.c
+++ code/lec10/myrealloc.c
@@ -2,7 +2,11 @@ void * myrealloc(void* old, size_t newsize) {
   if(!old) {return malloc(newsize);}
 
   entry_t* oldentry = ((entry_t*)old) - 1;
+  if(newsize < oldentry->size) {oldentry->size = newsize NO unless you also do block splitting; return old;}
+  //TODO are we at the top of the head if so just extend the heap
+  // then oldentry->size = newsize
   
+  // How can we avoid this slow path?
   void* new = malloc(newsize);
   memcpy(new,old, min(size,oldentry->size));
   free(old); old = NULL;
